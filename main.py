00fba7e34992314c41428ef07f48a09d697c6947
EZRA-DVLPR <derpisontr@gmail.com>
2025-11-03 14:48:36 -0800

    working code. need to fix capitalized letters, symbols, and make exe

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 from pynput.keyboard import Key, Listener
+from playsound3 import playsound
+import threading
+
+alphabet = [chr(i) for i in range(ord("a"), ord("z") + 1)]
+
+
+def play_sound(path):
+    playsound(path)
 
 
 def on_press(key):
-    print("{0} pressed".format(key))
+    try:
+        i = alphabet.index(key.char)
+
+        # out0{INDEX} where index is digits
+        name = f"out0{i:02d}"
+
+        # ./sounds/NAME/.wav
+        path = f"./sounds/{name}.wav"
+
+        print(f"Playing {path}")
+        # new thread to play multiple sounds at once
+        threading.Thread(target=play_sound, args=(path,), daemon=True).start()
+
+    except (AttributeError, ValueError):
+        # ignore all non-alpha keys
+        pass
 
 
 def on_release(key):
-    print("{0} release".format(key))
     if key == Key.esc:
         return False
 
